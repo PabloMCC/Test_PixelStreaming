45fea02a71775a95d9af376ab670bae8118dcf4e
PabloMCC <pablomoisescc@gmail.com>
2023-10-18 15:20:32 +0200

    Initial commit

---
+++ Source/Test_PixelStreaming/Test_PixelStreamingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Test_PixelStreamingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_PIXELSTREAMING_API ATest_PixelStreamingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
